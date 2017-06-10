3d28d31b9325f8a33e1446b079c0c05a8ea220a9
WilliamBarela <william.barela.dev@gmail.com>
2017-06-10 13:31:03 +0900

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,2 +1,37 @@
 class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    binding.pry
+    @figure = Figure.find_or_create_by(params[:figure])
+    @figure.landmarks << Landmark.find_or_create_by(params[:landmark]) if !!params[:landmark][:name]
+    @figure.titles << Title.find_or_create_by(params[:title]) if !!params[:title][:name]
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    
+
+    redirect to "/figures/#{params[:id]}"
+  end
 end
+
+
